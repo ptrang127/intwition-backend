3ea952fd754f142a953e9b0acdcaf72e46cca2e6
Phi Trang <p.trang127@gmail.com>
2020-08-19 22:14:39 -0600

    Update analyze.js

--- intwition/routes/analyze.js
+++ intwition/routes/analyze.js
@@ -5,35 +5,36 @@ var SentimentService = require('../services/sentiment-service');
 var CloudService = require('../services/cloud-service');
 
 /* GET sentiment given a term. */
-router.get('/term/:term', function (req, res, next) {
-  twitter.get('search/tweets', { q: req.params.term + " -filter:retweets", lang: 'en', count: 100, tweet_mode: 'extended' }, function (error, tweets, response) {
+router.get('/term/:term', function(req, res, next) {
+    twitter.get('search/tweets', { q: req.params.term + " -filter:retweets", lang: 'en', count: 100, tweet_mode: 'extended' }, function(error, tweets, response) {
 
-    // get all tweets and store in array
-    let tweet_array = tweets.statuses.map(tweet => {
-      return tweet.full_text;
+        // get all tweets and store in array
+        let tweet_array = tweets.statuses.map(tweet => {
+            return tweet.full_text;
+        });
+
+        let sentiment_result = SentimentService.getSentiment(tweet_array);
+        let cloud_result = CloudService.getCloud(tweet_array);
+
+        res.send({
+            sentiment: sentiment_result,
+            cloud: cloud_result,
+            tweets: tweet_array,
+            actual_tweets: tweets
+        });
+
     });
-
-    let sentiment_result = SentimentService.getSentiment(tweet_array);
-    let cloud_result = CloudService.getCloud(tweet_array);
-
-    res.send({
-      sentiment: sentiment_result,
-      cloud: cloud_result,
-      tweets: tweet_array
-    });
-
-  });
 
 });
 
 /* GET sentiment given an account. */
-router.get('/account/:account', function (req, res, next) {
-  res.send(req.params.account);
+router.get('/account/:account', function(req, res, next) {
+    res.send(req.params.account);
 });
 
 /* GET sentiment given a hashtag. */
-router.get('/hashtag', function (req, res, next) {
-  res.send(req.params.hashtag);
+router.get('/hashtag', function(req, res, next) {
+    res.send(req.params.hashtag);
 });
 
 module.exports = router;
